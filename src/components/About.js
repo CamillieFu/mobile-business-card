1f7a7f5c73d2bcdd03e1f9336f1f59d9f4e19a0c
Camille Fujisaki <cae224@cornell.edu>
2023-03-02 19:47:16 +0900

    update box shadow

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,12 @@ import AvcImage from "../images/avc.jpg"
 import Mamoru from "../images/mamoru.jpg"
 import '../assets/button.css'
 import '../assets/about.css'
-import Video from '../images/2bridal.mov';
+import Quiz from '../images/quiz.png';
+import TwoBridal from '../images/2bridal.png';
+import Tenzies from '../images/tenzies.png';
+import Notepad from '../images/note.png';
+
+
 
 export default function About(props) {
     return(
@@ -12,7 +17,8 @@ export default function About(props) {
         <p className="about-introduction-text">
           Greetings! I am a full-stack developer with a background in education, psychology and human development.
           I love researching, solving difficult problems, and creating beautiful products ✨ I am currently working with Ruby on Rails, JavaScript, React.js, and Next.js.
-          Constantly evolving and developing my skills while keeping my eye out for the next thing in web development.
+          Of course, I also have experience with HTML, CSS, Boostrap, Tailwind, and other front-end frameworks!
+          I am constantly evolving and developing my skills while keeping my eye out for the next thing in web development.
         </p>
         <h4 className="about-introduction-text">Check out some of my solo projects and freelance work below 👇🏼👇🏼👇🏼</h4>
         {/* projects */}
@@ -47,13 +53,44 @@ export default function About(props) {
           </div>
         </div>
         <div className="card-collection-article">
-          <video src={Video} width="600" height="300" controls="controls" autoplay="true" />
+          <img src={TwoBridal} alt="2bridal dress page" className="card-img"/>
+          {/* <video src={Video} width="600" height="300" controls="controls" autoplay="true" /> */}
           <div className="card-text">
             <a href="https://twobridal.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="card-title"><h1>2Bridal</h1></a>
             <p>2Bridal is an app for renting and borrowing used wedding dresses. It is simple, easy to browse and navigate, and fully responsive!
-              Made with: Ruby on Rails, CSS, Javascript
+              Made with: Ruby on Rails, CSS and Javascript.
             </p>
-            <a href="https://chirpy-camilliefu.herokuapp.com/" className="btn-green" target="_blank" rel="noopener noreferrer">See App</a>
+            <a href="https://twobridal.herokuapp.com/" className="btn-green" target="_blank" rel="noopener noreferrer">See App</a>
+          </div>
+        </div>
+        <div className="card-collection-article">
+          <img src={Quiz} alt="Quizzism app page" className="card-img"/>
+          <div className="card-text">
+            <a href="https://camilliefu.github.io/Quizzism/" target="_blank" rel="noopener noreferrer" className="card-title"><h1>Quizzism</h1></a>
+            <p>This app is a very simple trivia game made using React, CSS and a quiz API. It is a very straightforward and addictive game!
+              Of course, it is fully responsive and the design and graphics adjust to any screen size, so it can be enjoyed equally well on an SE or an ipad mini.
+            </p>
+            <a href="https://camilliefu.github.io/Quizzism/" className="btn-green" target="_blank" rel="noopener noreferrer">See App</a>
+          </div>
+        </div>
+        <div className="card-collection-article">
+          <img src={Tenzies} alt="Tenzies app page" className="card-img"/>
+          <div className="card-text">
+            <a href="https://camilliefu.github.io/tenzies/" target="_blank" rel="noopener noreferrer" className="card-title"><h1>Tenzies</h1></a>
+            <p>This app is a simple game, "Tenzies" built using React. The object of the game is to roll all the dice until every die is the same number.
+              The game is designed to look clean on both desktop and mobile, with little change in the look and feel of the UI.
+            </p>
+            <a href="https://camilliefu.github.io/tenzies/" className="btn-green" target="_blank" rel="noopener noreferrer">See App</a>
+          </div>
+        </div>
+        <div className="card-collection-article">
+          <img src={Notepad} alt="Notes app page" className="card-img"/>
+          <div className="card-text">
+            <a href="https://camilliefu.github.io/notepad/" target="_blank" rel="noopener noreferrer" className="card-title"><h1>Notepad</h1></a>
+            <p>This app is a simple note-taking app built using React and utilizes local storage. The design is minimal, cutesy (have you noticed I like purple?)
+              and clutter-free. The size of the note-taking space and side bar are fully adjustable by the user.
+            </p>
+            <a href="https://camilliefu.github.io/notepad/" className="btn-green" target="_blank" rel="noopener noreferrer">See App</a>
           </div>
         </div>
       </div>
